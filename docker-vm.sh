be8b5204167123ce7373162232cdd8514507df9e
tb <tberlen@gmail.com>
2025-04-09 09:44:24 -0400

    Update docker-vm.sh

--- docker-vm.sh
+++ docker-vm.sh
@@ -435,7 +435,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
-  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=2G \
+  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=12G \
   -boot order=scsi0 \
   -serial0 socket >/dev/null
 qm resize $VMID scsi0 18G >/dev/null
